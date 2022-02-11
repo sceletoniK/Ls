3269b54219eaff5f6c1cba747c25937f81b9c8fe
sceletoniK <L.s1n56@yandex.ru>
2022-02-11 23:40:53 +0300

    Страница main

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+
+    $l = "Вы не вошли :(";
+    session_start();
+
+    if(isset($_SESSION["login"]) && isset($_SESSION["password"]))
+    {
+        $l = "Приветствую, ".$_SESSION["login"]."!";
+    }
+
+    require("header.html");
+?>
+<main class="px-3">
+    <div class="card text-white bg-dark mb-3 d-inline-block" style="max-width: 20rem;">
+    <div class="card-body">
+        <h4 class="card-title">
+            <?php print($l); ?>
+        </h4>
+
+        <?php
+            if(isset($_SESSION["login"]) && isset($_SESSION["password"]))
+            {
+                ?>
+                    <form class="form-inline">
+                        <button type="submit" value="del" class="btn btn-primary mb-2">Выйти</button>
+                    </form>
+                <?php
+            }
+        ?>
+    </div>
+    </div>
+</main>
+<?php
+    require("footer.html");
+?>
