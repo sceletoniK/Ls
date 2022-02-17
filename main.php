1ecf475f3fb0c71b6554ebd663bcdc8cb2a06aa6
sceletoniK <L.s1n56@yandex.ru>
2022-02-17 23:52:57 +0300

    Добавил карточки: добавление, изменение, удаление

--- main.php
+++ main.php
@@ -12,12 +12,33 @@
     if(isset($_SESSION["login"]) && isset($_SESSION["password"]))
     {
         $l = "Приветствую, ".$_SESSION["login"]."!";
+
+        try 
+        {
+            $db = new PDO('mysql:host=localhost;dbname=users', 'root', '');
+        } catch (PDOException $e) 
+        {
+            print "Error!: ".$e->getMessage();
+            die();
+        }
+
+        if(isset($_REQUEST["add"]))
+        {
+            $stmt = $db->prepare("INSERT INTO `card` (`id`, `login`, `name`, `description`) VALUES (?, ?, ?, ?);");
+            $stmt->execute([NULL,$_SESSION["login"],$_REQUEST["name"],$_REQUEST["desc"]]);
+            header("location: main.php");
+        }
+
+        if(isset($_REQUEST["del"]))
+        {
+            $stmt = $db->prepare("DELETE FROM `card` WHERE `card`.`id` = ?");
+            $stmt->execute([$_REQUEST["del"]]);
+        }
+
     }
-
-
     require("header.html");
 ?>
-<main >
+<main>
     <div class="card text-white bg-dark mb-3 d-inline-block border-light" style="max-width: 20rem;">
     <div class="card-body">
         <h4 class="card-title">
@@ -31,7 +52,6 @@
                     <form class="form-inline">
                         <div class="d-flex justify-content-around align-items-center">
                             <button type="submit" name="sub" value="del" class="btn btn-primary btn-secondary mb-2">Выйти</button>
-                            <button type="submit" name="add" value="new" class="btn btn-primary btn-secondary mb-2">Добавить</button>
                         </div>
                     </form>
 
@@ -39,29 +59,61 @@
                     </div>
                     <div class="album py-5 text-dark bg-dark">
                     <div class="container">
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                            
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <?php
+                $stmt = $db->prepare("SELECT * FROM card WHERE `login` = ?");
+                $stmt->execute([$_SESSION["login"]]);
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+                {
+                    ?>
                             <div class="col">
                             <div class="card shadow-sm">
+                                <form style="margin: 0">
                                 <div class="card-header" role="tab" id="headingOne">
                                     <div class="d-flex justify-content-between align-items-center">
                                         <h5 class="mb-0">
-                                            Aboba
+                                            <?php print($row['name']); ?>
                                         </h5>
 
                                         <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                                            
+                                            <button type="button" class="btn btn-sm btn-outline-secondary"><a class="text-decoration-none" style="color: inherit" href="edit.php?l=<?php print($row['id']);?>">Edit</a></button>
+                                            <button type="submit" name="del" value="<?php print($row['id']);?>" class="btn btn-sm btn-outline-secondary">Delete</button>
+                                            
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-body">
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" style="resize: none;" readonly></textarea>
+                                    <textarea class="form-control " id="exampleFormControlTextarea1" rows="3" style="resize: none" readonly><?php print($row['description']);?></textarea>
+                                </div>
+                                </form>
+                            </div>
+                            </div>
+                    <?php
+                } 
+                ?>       
+                <div class="col">
+                <div class="card shadow-sm">
+                    <form style="margin: 0" method="POST">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h5 class="mb-0">
+                                    <input type="text" name="name">
+                                </h5>
+
+                                <div class="btn-group">
+                                    <button type="submit" name="add" value="new" class="btn btn-sm btn-outline-secondary">Add</button>
                                 </div>
                             </div>
-                            </div>
-                            
                         </div>
+                        <div class="card-body">
+                            <textarea class="form-control" name="desc" id="exampleFormControlTextarea1" rows="3" style="resize: none;"></textarea>
+                        </div>
+                    </form>
+                </div>
+                </div>
+
+                </div>
                 <?php
             }
         ?>
