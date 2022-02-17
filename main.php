1cd0e356b3c16b515c1ff96cb58f9550a5c68701
sceletoniK <L.s1n56@yandex.ru>
2022-02-17 10:20:33 +0300

    add cards

--- main.php
+++ main.php
@@ -17,7 +17,7 @@
 
     require("header.html");
 ?>
-<main class="px-3">
+<main >
     <div class="card text-white bg-dark mb-3 d-inline-block border-light" style="max-width: 20rem;">
     <div class="card-body">
         <h4 class="card-title">
@@ -29,8 +29,39 @@
             {
                 ?>
                     <form class="form-inline">
-                        <button type="submit" name="sub" value="del" class="btn btn-primary btn-secondary mb-2">Выйти</button>
+                        <div class="d-flex justify-content-around align-items-center">
+                            <button type="submit" name="sub" value="del" class="btn btn-primary btn-secondary mb-2">Выйти</button>
+                            <button type="submit" name="add" value="new" class="btn btn-primary btn-secondary mb-2">Добавить</button>
+                        </div>
                     </form>
+
+                    </div>
+                    </div>
+                    <div class="album py-5 text-dark bg-dark">
+                    <div class="container">
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                            
+                            <div class="col">
+                            <div class="card shadow-sm">
+                                <div class="card-header" role="tab" id="headingOne">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <h5 class="mb-0">
+                                            Aboba
+                                        </h5>
+
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" style="resize: none;" readonly></textarea>
+                                </div>
+                            </div>
+                            </div>
+                            
+                        </div>
                 <?php
             }
         ?>
