db987fe90574802dc19e079c14b2728b930f856d
sceletoniK <L.s1n56@yandex.ru>
2022-02-17 23:54:12 +0300

    Экспорт бд

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 17 2022 г., 05:13
+-- Время создания: Фев 17 2022 г., 21:53
 -- Версия сервера: 10.4.21-MariaDB
 -- Версия PHP: 8.0.12
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- База данных: `users`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `card`
+--
+
+CREATE TABLE `card` (
+  `id` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `card`
+--
+
+INSERT INTO `card` (`id`, `login`, `name`, `description`) VALUES
+(26, 'admin', 'Кыся', 'Абобы');
 
 -- --------------------------------------------------------
 
@@ -45,10 +65,37 @@ INSERT INTO `user` (`login`, `password`) VALUES
 --
 
 --
+-- Индексы таблицы `card`
+--
+ALTER TABLE `card`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `card_user_fk` (`login`);
+
+--
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `card`
+--
+ALTER TABLE `card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `card`
+--
+ALTER TABLE `card`
+  ADD CONSTRAINT `card_user_fk` FOREIGN KEY (`login`) REFERENCES `user` (`login`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
